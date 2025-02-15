21d3a877295dced3a8fe001d2abbd3604df6ff27
Joseph C. Miller, II <josephcmiller2@gmail.com>
2025-02-15 15:34:12 -0700

    Dynamic loading of fetch

--- backend/src/controllers/masterController.ts
+++ backend/src/controllers/masterController.ts
@@ -2,24 +2,48 @@
 
 import { Request, Response } from 'express';
 import * as fs from 'fs';
-import * as path from 'path';
-import { config } from '../app'; // Import the config from app.ts
+import { config } from '../app';
 
-export const getMasterDocument = (req: Request, res: Response) => {
+const loadMasterDocumentFile = (fileUri: string): Promise<any> => {
+    const filePath = fileUri.slice(7);
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading master document:', err);
+                return reject(new Error('Error reading master document'));
+            }
+            resolve(JSON.parse(data));
+        });
+    });
+};
+
+const loadMasterDocumentURI = async (uri: string): Promise<any> => {
+    const fetch = (await import('node-fetch')).default; // Dynamic import
+    const response = await fetch(uri);
+    if (!response.ok) {
+        throw new Error('Error fetching master document from URI');
+    }
+    return response.json();
+};
+
+export const getMasterDocument = async (req: Request, res: Response) => {
     if (!config.masterDocumentUri) {
         throw new Error('masterDocumentUri is not defined in the configuration.');
     }
 
-    // if filePath begins with file:/// then remove the file:// part
-    if (config.masterDocumentUri.startsWith('file://')) {
-        const filePath = config.masterDocumentUri.slice(7);
-
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                console.error('Error reading master document:', err);
-                return res.status(500).json({ message: 'Error reading master document' });
-            }
-            res.json(JSON.parse(data));
-        });
+    try {
+        let data;
+        if (config.masterDocumentUri.startsWith('file:///')) {
+            data = await loadMasterDocumentFile(config.masterDocumentUri);
+        } else {
+            data = await loadMasterDocumentURI(config.masterDocumentUri);
+        }
+        res.json(data);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: 'An unknown error occurred' });
+        }
     }
 }; 
